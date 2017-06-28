649af08cb2b967b9d08efedb1f6f301ef03bfa7b
isyuun <isyuun@gmail.com>
2017-06-28 12:57:54 +0900

    initial project

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+	public GameObject target;
+	NavMeshAgent agent;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.destination = target.transform.position;
+		animator.SetFloat("Speed", agent.velocity.magnitude);
+	}
+}
---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+	public string nextSceneName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Submit")) {
+			//Application.LoadLevel(nextSceneName);
+            SceneManager.LoadScene(nextSceneName);
+		}
+	}
+}
